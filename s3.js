a210de9ed8e6525139271d1c7b1bf5e5146fc976
Luke Ferris <luke.ferris@gmail.com>
2024-02-26 15:07:26 -0500

    Added signed url api elements for s3

---
+++ s3.js
@@ -0,0 +1,40 @@
+import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+
+// Initialize the S3 client
+const s3Client = new S3Client({ region: "eu-central-1" }); // Replace 'YOUR_REGION' with your S3 bucket's region
+const bucketName = process.env.BUCKET_NAME;
+
+const handler = async (event) => {
+  // Extracting the filename from the query string parameters
+  const objectKey = event.queryStringParameters?.filename || "defaultFileName";
+
+  const command = new PutObjectCommand({
+    Bucket: bucketName,
+    Key: objectKey,
+  });
+
+  try {
+    const url = await getSignedUrl(s3Client, command, { expiresIn: 60 * 5 }); // URL expiry time in seconds
+    return {
+      statusCode: 200,
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        message: "Presigned URL generated successfully",
+        uploadUrl: url,
+      }),
+    };
+  } catch (err) {
+    console.error(err);
+    return {
+      statusCode: 500,
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        message: "Error generating presigned URL",
+        error: err.message,
+      }),
+    };
+  }
+};
+
+export { handler };
