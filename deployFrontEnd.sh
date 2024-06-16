8165a2eadd29628427a7954ced50b5c5b6b7f6ff
Luke Ferris <luke.ferris@gmail.com>
2024-06-16 09:06:19 +0200

    Updated deployFrontEnd to use correct prod folder

--- deployFrontEnd.sh
+++ deployFrontEnd.sh
@@ -3,14 +3,11 @@
 # Get cloudfront distribution id from AWS
 CLOUDFRONT_DISTRIBUTION_ID=$(aws cloudformation describe-stack-resource --region=eu-central-1 --stack-name=o-[SOLUTION_ID_SHORT]-prod --logical-resource-id=SiteCdn --output=text --query=StackResourceDetail.PhysicalResourceId)
 
-# make a copy of the current react-website folder and all of its contents
-cp -r react-website/ react-website-deploy/
+cd react-website/
 
-cd react-website-deploy/
+vite build --mode prod
 
-vite build
-
-cd dist/
+cd prod/
 
 aws s3 sync . s3://o-[SOLUTION_ID_SHORT]-prod-origin-public-site/
 aws cloudfront create-invalidation --distribution-id=$CLOUDFRONT_DISTRIBUTION_ID --paths="/"
