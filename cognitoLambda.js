10e0ddb40dd6f4f60838a290c880b5df93ee2f24
Luke Ferris <luke.ferris@gmail.com>
2024-07-22 18:03:50 +0200

    Improved cognitoLambda to include comments and the ability to update groups

--- cognitoLambda.js
+++ cognitoLambda.js
@@ -1,13 +1,49 @@
+/**
+ * AWS Cognito User Management Lambda Function
+ *
+ * This Lambda function provides CRUD operations for managing users in AWS Cognito.
+ * It allows creating, reading, updating, and deleting users, as well as managing their group memberships.
+ *
+ * userData Structure:
+ * For POST (create) and PUT (update) operations, the userData should be structured as follows:
+ *
+ * {
+ *   "username": "string",  // Required for POST, not used for PUT (derived from path)
+ *   "password": "string",  // Required for POST, optional for PUT
+ *   "groups": ["string"]   // Optional. Array of group names the user should belong to
+ * }
+ *
+ * Notes:
+ * - For POST (create user):
+ *   - 'username' and 'password' are required
+ *   - 'groups' is optional
+ * - For PUT (update user):
+ *   - 'password' is optional. If provided, it will update the user's password
+ *   - 'groups' is optional. If provided, it will update the user's group memberships
+ * - This implementation does not allow setting or updating other user attributes
+ * - Group management is handled by adding/removing the user from specified groups
+ *
+ * Environment Variables:
+ * - USER_POOL_ID: The ID of the Cognito User Pool to operate on
+ *
+ * API Endpoints:
+ * - GET /users : List all users
+ * - GET /users/{username} : Get details of a specific user
+ * - POST /users : Create a new user
+ * - PUT /users/{username} : Update an existing user
+ * - DELETE /users/{username} : Delete a user
+ */
+
 import {
   CognitoIdentityProviderClient,
   AdminCreateUserCommand,
   AdminDeleteUserCommand,
-  AdminUpdateUserAttributesCommand,
   AdminSetUserPasswordCommand,
-  SignUpCommand,
-  AdminConfirmSignUpCommand,
   ListUsersCommand,
   AdminGetUserCommand,
+  AdminAddUserToGroupCommand,
+  AdminRemoveUserFromGroupCommand,
+  AdminListGroupsForUserCommand,
 } from "@aws-sdk/client-cognito-identity-provider";
 
 const cognitoClient = new CognitoIdentityProviderClient({
@@ -15,7 +51,6 @@ const cognitoClient = new CognitoIdentityProviderClient({
 });
 
 const userPoolId = process.env.USER_POOL_ID;
-const clientId = process.env.CLIENT_ID;
 
 export async function handler(event, context) {
   try {
@@ -54,10 +89,12 @@ async function listUsers() {
   const command = new ListUsersCommand(params);
   const response = await cognitoClient.send(command);
 
-  const users = response.Users.map((user) => ({
-    username: user.Username,
-    attributes: user.Attributes,
-  }));
+  const users = await Promise.all(
+    response.Users.map(async (user) => ({
+      username: user.Username,
+      groups: await getUserGroups(user.Username),
+    }))
+  );
 
   return {
     statusCode: 200,
@@ -75,75 +112,51 @@ async function getUser(username) {
   const command = new AdminGetUserCommand(params);
   const user = await cognitoClient.send(command);
 
+  const groups = await getUserGroups(username);
+
   return {
     statusCode: 200,
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({
       username: user.Username,
-      attributes: user.UserAttributes,
+      groups: groups,
     }),
   };
 }
 
 async function createUser(userData) {
-  const { username, password, ...attributes } = userData;
+  const { username, password, groups } = userData;
 
-  if (password) {
-    // Self-registration
-    const signUpParams = {
-      ClientId: clientId,
-      Username: username,
-      Password: password,
-      UserAttributes: Object.entries(attributes).map(([Name, Value]) => ({
-        Name,
-        Value,
-      })),
-    };
+  if (!username || !password) {
+    return badRequest("Username and password are required");
+  }
 
-    await cognitoClient.send(new SignUpCommand(signUpParams));
+  const params = {
+    UserPoolId: userPoolId,
+    Username: username,
+    TemporaryPassword: password,
+  };
 
-    const confirmParams = {
-      UserPoolId: userPoolId,
-      Username: username,
-    };
+  await cognitoClient.send(new AdminCreateUserCommand(params));
 
-    await cognitoClient.send(new AdminConfirmSignUpCommand(confirmParams));
-  } else {
-    // Admin creation
-    const params = {
-      UserPoolId: userPoolId,
-      Username: username,
-      TemporaryPassword: generateTemporaryPassword(),
-      UserAttributes: Object.entries(attributes).map(([Name, Value]) => ({
-        Name,
-        Value,
-      })),
-    };
+  await resetPassword(username, password);
 
-    await cognitoClient.send(new AdminCreateUserCommand(params));
+  if (groups && groups.length > 0) {
+    await updateUserGroups(username, groups);
   }
 
   return success(`User ${username} created successfully`);
 }
 
 async function updateUser(username, userData) {
-  const { password, ...attributes } = userData;
+  const { password, groups } = userData;
 
   if (password) {
     await resetPassword(username, password);
   }
 
-  if (Object.keys(attributes).length > 0) {
-    const params = {
-      UserPoolId: userPoolId,
-      Username: username,
-      UserAttributes: Object.entries(attributes).map(([Name, Value]) => ({
-        Name,
-        Value,
-      })),
-    };
-
-    await cognitoClient.send(new AdminUpdateUserAttributesCommand(params));
+  if (groups) {
+    await updateUserGroups(username, groups);
   }
 
   return success(`User ${username} updated successfully`);
@@ -170,8 +183,55 @@ async function resetPassword(username, newPassword) {
   await cognitoClient.send(new AdminSetUserPasswordCommand(params));
 }
 
-function generateTemporaryPassword() {
-  return Math.random().toString(36).slice(-8);
+async function updateUserGroups(username, newGroups) {
+  const currentGroups = await getUserGroups(username);
+
+  const groupsToAdd = newGroups.filter(
+    (group) => !currentGroups.includes(group)
+  );
+  const groupsToRemove = currentGroups.filter(
+    (group) => !newGroups.includes(group)
+  );
+
+  for (const group of groupsToAdd) {
+    await addUserToGroup(username, group);
+  }
+
+  for (const group of groupsToRemove) {
+    await removeUserFromGroup(username, group);
+  }
+}
+
+async function getUserGroups(username) {
+  const params = {
+    UserPoolId: userPoolId,
+    Username: username,
+  };
+
+  const command = new AdminListGroupsForUserCommand(params);
+  const response = await cognitoClient.send(command);
+
+  return response.Groups.map((group) => group.GroupName);
+}
+
+async function addUserToGroup(username, groupName) {
+  const params = {
+    UserPoolId: userPoolId,
+    Username: username,
+    GroupName: groupName,
+  };
+
+  await cognitoClient.send(new AdminAddUserToGroupCommand(params));
+}
+
+async function removeUserFromGroup(username, groupName) {
+  const params = {
+    UserPoolId: userPoolId,
+    Username: username,
+    GroupName: groupName,
+  };
+
+  await cognitoClient.send(new AdminRemoveUserFromGroupCommand(params));
 }
 
 function success(message) {
