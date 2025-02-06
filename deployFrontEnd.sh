7c253388ee636499f3981e7bec1f4ce79692a474
Luke Ferris <luke.ferris@gmail.com>
2025-02-06 07:45:38 +0100

    Updated deploy front end script to use siteCdn logical id to find cloudfront

--- deployFrontEnd.sh
+++ deployFrontEnd.sh
@@ -4,11 +4,11 @@
 REGION="eu-central-1"
 PARENT_STACK="o[SOLUTION_ID_SHORT]-[USERNAME_LOWER_LAST3]-p"
 
-# Find the nested stack that contains CloudFront (assuming it has 'CloudFront' in its name)
+# Find the nested stack that contains the SiteCdn resource
 NESTED_STACK=$(aws cloudformation list-stack-resources \
-    --region $REGION \
-    --stack-name $PARENT_STACK \
-    --query "StackResourceSummaries[?ResourceType=='AWS::CloudFormation::Stack' && contains(PhysicalResourceId, 'CloudFront')].PhysicalResourceId" \
+    --region "$REGION" \
+    --stack-name "$PARENT_STACK" \
+    --query "StackResourceSummaries[?LogicalResourceId=='SiteCdn'].PhysicalResourceId" \
     --output text)
 
 # Ensure the nested stack was found
@@ -21,7 +21,7 @@ echo "Found nested stack: $NESTED_STACK"
 
 # Now find CloudFront Distribution ID from the nested stack
 CLOUDFRONT_DISTRIBUTION_ID=$(aws cloudformation describe-stack-resources \
-    --region $REGION \
+    --region "$REGION" \
     --stack-name "$NESTED_STACK" \
     --query "StackResources[?LogicalResourceId=='SiteCdn'].PhysicalResourceId" \
     --output text)
@@ -35,18 +35,18 @@ fi
 echo "CloudFront Distribution ID: $CLOUDFRONT_DISTRIBUTION_ID"
 
 # Navigate to the React project directory
-cd ../../react-website/
+cd ../../react-website/ || exit
 
 # Build the React project using Vite
 vite build --mode prod
 
 # Change to the production directory
-cd prod/
+cd prod/ || exit
 
 # Sync built files to the S3 bucket
 aws s3 sync . s3://[SOLUTION_ID_SHORT]-productioncloudfronts3/
 
 # Invalidate the CloudFront cache
-aws cloudfront create-invalidation --distribution-id="$CLOUDFRONT_DISTRIBUTION_ID" --paths "/"
+aws cloudfront create-invalidation --distribution-id "$CLOUDFRONT_DISTRIBUTION_ID" --paths "/*"
 
 echo "Deployment complete!"
